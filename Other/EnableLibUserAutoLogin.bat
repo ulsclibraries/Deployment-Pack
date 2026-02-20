87cc7a318ab5f2af9bc9d7ada3a80f1dbb1bacf0
ulsclibraries <ulsclibraries@hitechsupport.com.au>
2026-02-20 16:32:40 +1100

    Added all registry params needed to enable Autologin for LibUser

--- Other/EnableLibUserAutoLogin.bat
+++ Other/EnableLibUserAutoLogin.bat
@@ -1,3 +1,6 @@
 @echo off
 :: Windows 11 Pro - Enable LibUser Autologin
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon   /t REG_SZ /d 1             /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName  /t REG_SZ /d "LibUser"     /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword  /t REG_SZ /d ""            /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /t REG_SZ /d "%COMPUTERNAME%" /f >nul 2>&1
