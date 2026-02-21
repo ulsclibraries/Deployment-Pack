b9939658eb611f8d83d05d8d72c0c9e77cbb6267
ulsclibraries <ulsclibraries@hitechsupport.com.au>
2026-02-21 19:20:58 +1100

    Refactored batch files for disable autologin

--- Other/DisableLibUserAutoLogin.bat
+++ Other/DisableLibUserAutoLogin.bat
@@ -1,3 +1,6 @@
 @echo off
 :: Windows 11 Pro - Disable LibUser Autologin
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 0 /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon    /t REG_SZ /d 0 /f >nul 2>&1
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName   /f >nul 2>&1
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword   /f >nul 2>&1
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /f >nul 2>&1
